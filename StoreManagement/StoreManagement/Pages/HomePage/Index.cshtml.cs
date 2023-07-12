45591d47148562644105d6e5eb0660589a9c5f77
congthinh.161 <congthinh.work161@gmail.com>
2023-07-12 16:47:39 +0700

    FE basic user and admin

---
+++ StoreManagement/StoreManagement/Pages/HomePage/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StoreManagement.Pages.HomePage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
